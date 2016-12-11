6d7a17f222af90284b5f29c9c28c25d7716e2e99
Nate Luzod <nate.luzod@mac.com>
2016-12-11 12:51:42 -1000

    AWS Integration, saving to bucket - WIP

--- fetch.sh
+++ fetch.sh
@@ -2,4 +2,25 @@
 
 NF=$(date +%s)
 
-wget http://www.esrl.noaa.gov/gmd/webdata/mlo/webcam/northcam.jpg && mv northcam.jpg public/img/$NF.jpg
+wget http://www.esrl.noaa.gov/gmd/webdata/mlo/webcam/northcam.jpg && mv northcam.jpg tmp/$NF.jpg
+
+cd tmp/
+
+file="$NF.jpg"
+
+key_id=$AWS_ACCESS_KEY_ID
+key_secret=$AWS_SECRET_KEY
+bucket="mauna-time-machine"
+content_type="application/octet-stream"
+date="$(LC_ALL=C date -u +"%a, %d %b %Y %X %z")"
+md5="$(openssl md5 -binary < "$file" | base64)"
+
+sig="$(printf "PUT\n$md5\n$content_type\n$date\n/$bucket/$file" | openssl sha1 -binary -hmac "$key_secret" | base64)"
+
+curl -T $file http://$bucket.s3.amazonaws.com/$path \
+    -H "Date: $date" \
+    -H "Authorization: AWS $key_id:$sig" \
+    -H "Content-Type: $content_type" \
+    -H "Content-MD5: $md5"
+
+rm -rf $NF.jpg
