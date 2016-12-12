816fcdeb02543a97aa7425f6a6b7ed28e776017e
Nate Luzod <nate.luzod@mac.com>
2016-12-11 20:09:50 -1000

    Pulling images from S3

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// echo getenv('AWS_ACCESS_KEY_ID');
-// echo getenv('AWS_SECRET_KEY');
-
 date_default_timezone_set('Pacific/Honolulu');
 
 require '../vendor/autoload.php';
@@ -10,7 +7,6 @@ require '../vendor/autoload.php';
 $app = new \Slim\App();
 
 require __DIR__ . '/../src/middleware.php';
-
 require __DIR__ . '/../src/routes.php';
 
 $app->run();
