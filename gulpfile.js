823b4c1eb3b5fa51009c889f5e6bc4165211e55d
Nate Luzod <nate.luzod@mac.com>
2016-12-26 12:38:53 -0800

    Converting to Laravel from Slim

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-'use strict';
+const elixir = require('laravel-elixir');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+require('laravel-elixir-vue-2');
 
-gulp.task('sass', function () {
-  return gulp.src('./src/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/'));
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ |
+ */
+
+elixir((mix) => {
+    mix.sass('app.scss')
+       .webpack('app.js');
 });
-
-gulp.task('watch', function () {
-  gulp.watch('./src/scss/*.scss', ['sass']);
-});
-
-gulp.task('default', ['watch']);
